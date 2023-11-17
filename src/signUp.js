66edf9f533a836d7f93b47f44c3794f3956d2727
PrasanthRajaratnam <100757951+PrasanthRajaratnam@users.noreply.github.com>
2023-11-17 20:21:02 +0530

    Update signUp.js

--- src/signUp.js
+++ src/signUp.js
@@ -22,12 +22,11 @@ function SignUp(){
             </div>
             <div className='text-light text-start col-sm right ms-5 pt-2 me-5'>
                 <h1 className='text-warning text-center'><i>E-kart</i></h1>
-                <h2><i>Embark on a Digital Odyssey: Your Premier Destination for Elevated E-Commerce Excellence</i></h2>
-                <h3><i>Welcome to the digital marketplace where desires meet convenience, and every click opens the door to a world of curated elegance</i></h3>
-                <h3><i>Step into a realm where style knows no bounds and every purchase is a statement of sophistication. Welcome to your online shopping sanctuary.</i></h3>
-                <h3><i>Step into a virtual boutique where every click is an invitation to discover the extraordinary. Your journey into the world of sophisticated online shopping starts now.</i></h3>
-                <h3><i>In the realm of pixels and possibilities, we invite you to explore a curated collection that transcends trends and redefines the art of online shopping.</i></h3>
-            </div>
+                <h2><i>
+"Culinary Exploration Awaits: Embark on a Gastronomic Journey with Our Recipe Finder – Sign Up Today for a World of Flavor!"</i></h2>
+                <h3><i>Tailored to your taste preferences, our algorithm delivers handpicked recipes, ensuring a delightful and personalized cooking experience.</i></h3>
+                <h3><i>Join a vibrant community of food enthusiasts, share your culinary creations, and engage in discussions with fellow members to elevate your cooking skills together.</i></h3>
+                </div>
         </div>
     )
 }
